d20f39630e092a4304cb3e1d60e07a012c897077
Bear R <bearrambo9@gmail.com>
2025-08-30 14:48:07 -0700

    Implement more filetype icons

--- webapp/src/components/FileExplorerLeaf/FileExplorerLeaf.jsx
+++ webapp/src/components/FileExplorerLeaf/FileExplorerLeaf.jsx
@@ -1,5 +1,21 @@
 import { Group } from "@mantine/core";
-import { IconFolderFilled, IconFileFilled } from "@tabler/icons-react";
+import {
+  IconFolderFilled,
+  IconFileFilled,
+  IconBrandPython,
+  IconFileTypeHtml,
+  IconFileTypeCss,
+  IconFileTypePdf,
+  IconFileTypeDoc,
+  IconFileTypeJpg,
+  IconFileTypePng,
+  IconPhoto,
+  IconVideo,
+  IconMusic,
+  IconFileTypeJs,
+  IconFileTypeTs,
+} from "@tabler/icons-react";
+import { useState } from "react";
 
 function FileExplorerLeaf({
   name,
@@ -8,6 +24,7 @@ function FileExplorerLeaf({
   isFolder,
   onContextMenu,
 }) {
+  const [fileExtension, setFileExtension] = useState("");
   const handleContextMenu = (event) => {
     event.preventDefault();
     event.stopPropagation();
@@ -19,6 +36,37 @@ function FileExplorerLeaf({
     };
 
     onContextMenu(event, item);
+  };
+
+  const getFileIcon = (fileName, isFolder) => {
+    if (isFolder) {
+      return <IconFolderFilled color={"yellow"} size={14} stroke={2.5} />;
+    }
+
+    const extension = fileName.split(".").pop()?.toLowerCase();
+
+    const iconMap = {
+      py: <IconBrandPython size={14} stroke={2.5} />,
+      js: <IconFileTypeJs size={14} stroke={2.5} />,
+      jsx: <IconFileTypeJs size={14} stroke={2.5} />,
+      ts: <IconFileTypeTs size={14} stroke={2.5} />,
+      tsx: <IconFileTypeTs size={14} stroke={2.5} />,
+      html: <IconFileTypeHtml size={14} stroke={2.5} />,
+      css: <IconFileTypeCss size={14} stroke={2.5} />,
+      pdf: <IconFileTypePdf size={14} stroke={2.5} />,
+      doc: <IconFileTypeDoc size={14} stroke={2.5} />,
+      docx: <IconFileTypeDoc size={14} stroke={2.5} />,
+      jpg: <IconFileTypeJpg size={14} stroke={2.5} />,
+      jpeg: <IconFileTypeJpg size={14} stroke={2.5} />,
+      png: <IconFileTypePng size={14} stroke={2.5} />,
+      gif: <IconPhoto size={14} stroke={2.5} />,
+      mp3: <IconVideo size={14} stroke={2.5} />,
+      wav: <IconMusic size={14} stroke={2.5} />,
+      mp4: <IconVideo size={14} stroke={2.5} />,
+      avi: <IconVideo size={14} stroke={2.5} />,
+    };
+
+    return iconMap[extension] || <IconFileFilled size={14} stroke={2.5} />;
   };
 
   return (
@@ -34,11 +82,7 @@ function FileExplorerLeaf({
       }}
       className="file-explorer-leaf"
     >
-      {isFolder ? (
-        <IconFolderFilled color={"yellow"} size={14} stroke={2.5} />
-      ) : (
-        <IconFileFilled size={14} stroke={2.5} />
-      )}
+      {getFileIcon(name, isFolder)}
       <span>{name}</span>
     </Group>
   );
